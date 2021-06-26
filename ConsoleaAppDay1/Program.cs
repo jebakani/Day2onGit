e3b758ba764b3599d4dcea71796b563130837c8f
jebakani ishwarya V <56194449+jebakani@users.noreply.github.com>
2021-06-26 10:06:07 +0530

    Update Program.cs

--- ConsoleaAppDay1/Program.cs
+++ ConsoleaAppDay1/Program.cs
@@ -7,11 +7,8 @@ namespace ConsoleaAppDay1
         static void Main(string[] args)
         {
             int number,number2;
-<<<<<<< HEAD
             int n1;
-=======
             int num1;
->>>>>>> 24178c14c157447227ef2c988544d8fcee2a3b49
             Console.WriteLine("Hello World!");
             Console.WriteLine("changes made in branch");
             Console.WriteLine("changes made in branch 2");
