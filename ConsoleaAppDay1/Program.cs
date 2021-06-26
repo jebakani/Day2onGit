0fd7efc0d8e7ae327f88f1103637bcf19f834530
Jebakani <vjishwarya@gmail.com>
2021-06-26 09:59:56 +0530

    [JEBAKANI]

--- ConsoleaAppDay1/Program.cs
+++ ConsoleaAppDay1/Program.cs
@@ -7,11 +7,8 @@ namespace ConsoleaAppDay1
         static void Main(string[] args)
         {
             int number,number2;
-<<<<<<< HEAD
             int n1;
-=======
-            int num1;
->>>>>>> 24178c14c157447227ef2c988544d8fcee2a3b49
+            int num1;     
             Console.WriteLine("Hello World!");
             Console.WriteLine("changes made in branch");
             Console.WriteLine("changes made in branch 2");
@@ -19,6 +16,7 @@ namespace ConsoleaAppDay1
             Console.WriteLine("Testing merging");
             Console.WriteLine("updated to version2.0");
             Console.WriteLine("hello");
+            Console.WriteLine("Hi");
         }
     }
 }
